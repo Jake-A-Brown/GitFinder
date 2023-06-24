620c46292a637296d331a683654db747b3331e25
Jake Brown <JakeBrown@jbrownjake.com>
2023-06-24 15:56:59 -0400

    initial

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/GitFinder/',
-  plugins: [react()]
+  plugins: [react()],
+  base:'/github-profile-finder/'
 })
